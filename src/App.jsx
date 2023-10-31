eb79e315d9c10da03629c9964a6c7dd704902410
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-10-31 15:27:17 +0530

    updation

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,26 @@ function App() {
   return (
     <>
    
-<div className='grid grid-cols-3'>
+<div className=' w-full h-full  lg:grid-cols-3 sm:grid  grid-cols-3  gap-3 p-3'>
 {data.map((item, index) => (
-  <div key={index} className= 'w-full h-1/2 p-3 '>
+  <div className= ' p-3  w-full  '>
      <div className='  w-full h-48 '>
-      <img className='object-cover rounded-md '  src={item.image} alt="" />
+      <img className='object-cover w-full h-full rounded-lg'  
+       src={item.image}
+       alt="" />
       </div>
       <div className='flex justify-around '>
-        <h1 className="text-md text-start font-bold " >
+        <h1 className="text-md font-mono text-start font-bold " >
           {item.name}
         </h1> 
-        <h1 className="text-md text-start font-bold " >
-          {item.price}
+        <h1 className="text-md font-mono text-start font-bold " >
+     Rs:{item.price}.00
         </h1> 
         </div>
     
   </div>
-     )
-     )}
+)
+)}
 
 </div>
     </>
